34f0dfcaf72136ce6c9f038ea990f44c0af828ba
Counterfe1t <karoljanbarcz@gmail.com>
2021-05-12 19:20:05 +0100

    Enable extended execution while app is minimized (UWP)

--- Pomodorek.UWP/MainPage.xaml.cs
+++ Pomodorek.UWP/MainPage.xaml.cs
@@ -1,9 +1,24 @@
-﻿namespace Pomodorek.UWP {
+﻿using System;
+using Windows.ApplicationModel.ExtendedExecution;
+
+namespace Pomodorek.UWP {
     public sealed partial class MainPage {
+
+        private ExtendedExecutionSession session = null;
+
         public MainPage() {
             InitializeComponent();
-
+            BeginExtendedExecution();
             LoadApplication(new Pomodorek.App());
+        }
+
+        private async void BeginExtendedExecution() {
+            var newSession = new ExtendedExecutionSession {
+                Reason = ExtendedExecutionReason.Unspecified,
+                Description = ""
+            };
+            session = newSession;
+            _ = await session.RequestExtensionAsync();
         }
     }
 }
