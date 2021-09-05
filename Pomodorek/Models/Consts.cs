311d840cb86765a82ae02b6dbeb5093b0c22cb09
Counterfe1t <karoljanbarcz@gmail.com>
2021-09-05 18:29:34 +0200

    Modifying Entry control appearance, minor cosmetic changes

--- Pomodorek/Models/Consts.cs
+++ Pomodorek/Models/Consts.cs
@@ -1,14 +1,14 @@
 ﻿namespace Pomodorek.Models {
     public class Consts {
-        public static readonly int FocusLength = 2; //25
-        public static readonly int ShortRestLength = 1; //5
-        public static readonly int LongRestLength = 2; //20
+        public static readonly int FocusLength = 25;
+        public static readonly int ShortRestLength = 5;
+        public static readonly int LongRestLength = 20;
 
         public static readonly string FocusModeLabel = "FOCUS";
         public static readonly string ShortRestModeLabel = "REST";
         public static readonly string LongRestModeLabel = "LONG REST";
 
-        public static readonly string FocusModeNotificationMessage = "Go back to work.";
+        public static readonly string FocusModeNotificationMessage = "Get back to work.";
         public static readonly string ShortRestModeNotificationMessage = "Take a short break.";
         public static readonly string LongRestModeNotificationMessage = "Take a long break.";
         public static readonly string SessionOverNotificationMessage = "Session has ended.";
