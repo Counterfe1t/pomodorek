b45aa45ef2719f10a339854a8b2ac7aecf617d13
Counterfe1t <karoljanbarcz@gmail.com>
2023-01-15 21:55:48 +0100

    Handle stepper visibility while timer is running

--- Pomodorek/Converters/NegationConverter.cs
+++ Pomodorek/Converters/NegationConverter.cs
@@ -7,10 +7,7 @@ namespace Pomodorek.Converters
         #region IValueConverter
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var booleanValue = (bool)value;
-            return !booleanValue;
-        }
+            => !(bool)value;
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
