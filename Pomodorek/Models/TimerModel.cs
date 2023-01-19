e5e315d1fcf42dbf6817a86e791a18bcbb85a0ea
Counterfe1t <karoljanbarcz@gmail.com>
2023-01-19 19:53:29 +0100

    Modify timer logic, create separate task to invoke a callback once every second until its token is canceled

--- Pomodorek/Models/TimerModel.cs
+++ Pomodorek/Models/TimerModel.cs
@@ -2,39 +2,30 @@
 {
     public class TimerModel
     {
-        private readonly IDispatcherTimer _timer;
         private readonly Action _callback;
         private static CancellationTokenSource _token;
 
         // todo: write unit tests
         public TimerModel(Action callback)
         {
-            _timer = Application.Current.Dispatcher.CreateTimer();
-            _timer.Interval = TimeSpan.FromMilliseconds(1000);
             _callback = callback;
             _token = new CancellationTokenSource();
         }
 
         public void Start()
         {
-            _timer.Tick += OnTickEvent;
-            _timer.Start();
+            var token = _token;
+            Task.Run(async () =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    await Task.Delay(1000);
+                    if (!token.IsCancellationRequested)
+                        _callback.Invoke();
+                }
+            });
         }
 
-        public void Stop()
-        {
-            // todo: stopping does not work od android
-            _token.Cancel();
-            Interlocked.Exchange(ref _token, new CancellationTokenSource());
-            _timer.Stop();
-            _timer.Tick -= OnTickEvent;
-        }
-
-        private void OnTickEvent(object e, EventArgs sender)
-        {
-            if (_token.IsCancellationRequested)
-                _timer.Stop();
-            _callback.Invoke();
-        }
+        public void Stop() => Interlocked.Exchange(ref _token, new CancellationTokenSource()).Cancel();
     }
 }
