516cacb744d79253f9a1dae57afe6a08958059eb
Counterfe1t <karoljanbarcz@gmail.com>
2023-02-15 19:45:33 +0100

    Simplify generic method call in SettingsService

--- Pomodorek/Services/SettingsService.cs
+++ Pomodorek/Services/SettingsService.cs
@@ -3,7 +3,7 @@
 public class SettingsService : ISettingsService
 {
     public T Get<T>(string key, T defaultValue) =>
-        Preferences.Default.Get<T>(key, defaultValue);
+        Preferences.Default.Get(key, defaultValue);
     public void Set<T>(string key, T value) =>
-        Preferences.Default.Set<T>(key, value);
+        Preferences.Default.Set(key, value);
 }
