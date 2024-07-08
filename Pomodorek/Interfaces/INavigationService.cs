f50f8dc27c5bdf33f747b2a0e52a90d91c5650e6
Counterfe1t <karoljanbarcz@gmail.com>
2024-07-09 00:27:09 +0200

    Minor clean up in navigation service

--- Pomodorek/Interfaces/INavigationService.cs
+++ Pomodorek/Interfaces/INavigationService.cs
@@ -3,12 +3,12 @@
 public interface INavigationService
 {
     /// <summary>
-    /// Navigate to the settings page.
-    /// </summary>
-    Task GoToSettingsPageAsync();
-
-    /// <summary>
     /// Navigate to the timer page.
     /// </summary>
     Task GoToTimerPageAsync();
+
+    /// <summary>
+    /// Navigate to the settings page.
+    /// </summary>
+    Task GoToSettingsPageAsync();
 }
--- Pomodorek/Services/NavigationService.cs
+++ Pomodorek/Services/NavigationService.cs
@@ -1,10 +1,8 @@
-﻿using Pomodorek.Interfaces;
-
-namespace Pomodorek.Services;
+﻿namespace Pomodorek.Services;
 
 public class NavigationService : INavigationService
 {
-    public async Task GoToSettingsPageAsync() => await Shell.Current.GoToAsync("//SettingsPage");
-
     public async Task GoToTimerPageAsync() => await Shell.Current.GoToAsync("//TimerPage");
+
+    public async Task GoToSettingsPageAsync() => await Shell.Current.GoToAsync("//SettingsPage");
 }
