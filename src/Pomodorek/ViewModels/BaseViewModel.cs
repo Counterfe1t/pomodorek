67b2ade9d66f69bfab39bdbe01abce38c855f3b8
Counterfe1t <karoljanbarcz@gmail.com>
2024-08-11 22:03:31 +0200

    Add IsNotBusy property to BaseViewModel

--- src/Pomodorek/ViewModels/BaseViewModel.cs
+++ src/Pomodorek/ViewModels/BaseViewModel.cs
@@ -3,10 +3,13 @@
 public partial class BaseViewModel : ObservableObject
 {
     [ObservableProperty]
-    private bool _isBusy = false;
-    
-    [ObservableProperty]
     private string _title;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+    private bool _isBusy = false;
+
+    public bool IsNotBusy => !IsBusy;
 
     public BaseViewModel(string title)
     {
