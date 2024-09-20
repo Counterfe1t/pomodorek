769f51e559c267593260208613fd129fbb9674cd
Counterfe1t <karoljanbarcz@gmail.com>
2024-09-20 23:58:37 +0200

    Add base class for unit testing

---
+++ tests/Pomodorek.Tests/UnitTests/SystemUnderTest.cs
@@ -0,0 +1,51 @@
+﻿namespace Pomodorek.Tests.UnitTests;
+
+public static class SystemUnderTest
+{
+    public static T? Is<T>(params object[] knownSubstitutions) where T : class
+    {
+        var args = CreateParameters<T>(knownSubstitutions);
+
+        return Activator.CreateInstance(typeof(T), args) as T;
+    }
+
+    private static object[] CreateParameters<T>(params object[] known) where T : class
+    {
+        var list = new List<object>();
+        var ci = typeof(T).GetConstructors().FirstOrDefault();
+
+        if (ci is null)
+        {
+            return [];
+        }
+
+        var arrayOfParams = ci.GetParameters().Select(p => p.ParameterType).ToArray();
+        var knownParams = known.Select((item) => new
+        {
+            Instance = item,
+            Interfaces = item.GetType().GetInterfaces(),
+        }).ToArray();
+
+        foreach (var param in arrayOfParams)
+        {
+            var paramMocked = knownParams.FirstOrDefault(knownParam => knownParam.Interfaces.Contains(param));
+
+            if (paramMocked is not null)
+            {
+                list.Add(paramMocked.Instance);
+            }
+            else
+            {
+                var mockedParam = typeof(Mock<>).MakeGenericType(param);
+                var mock = Activator.CreateInstance(mockedParam) as Mock;
+
+                if (mock is not null)
+                {
+                    list.Add(mock.Object);
+                }
+            }
+        }
+
+        return [.. list];
+    }
+}
