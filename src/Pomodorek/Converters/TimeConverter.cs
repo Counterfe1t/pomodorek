de32a861e1c4fea960ff40a110ad16f00cca3594
Counterfe1t <karoljanbarcz@gmail.com>
2024-09-23 15:00:29 +0200

    Simplify block body method to lambda expression

--- src/Pomodorek/Converters/TimeConverter.cs
+++ src/Pomodorek/Converters/TimeConverter.cs
@@ -2,15 +2,11 @@
 
 public class TimeConverter : IValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => FormatTime((int)value);
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+        FormatTime((int)value);
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
         throw new NotImplementedException();
 
-    public static string FormatTime(int seconds)
-    {
-        var minutes = seconds / 60;
-
-        return $"{(minutes < 10 ? "0" : "")}{minutes}:{((seconds % 60) < 10 ? "0" : "")}{seconds % 60}";
-    }
+    public static string FormatTime(int seconds) => TimeSpan.FromSeconds(seconds).ToString(@"mm\:ss");
 }
