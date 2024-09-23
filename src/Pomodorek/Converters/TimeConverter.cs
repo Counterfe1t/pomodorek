b0fd2e456dd61b84828b000e0c5b652b09afaadf
Counterfe1t <karoljanbarcz@gmail.com>
2024-09-23 15:00:29 +0200

    Modify time converter logic to use TimeSpan class and string formatting

--- src/Pomodorek/Converters/TimeConverter.cs
+++ src/Pomodorek/Converters/TimeConverter.cs
@@ -2,15 +2,16 @@
 
 public class TimeConverter : IValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => FormatTime((int)value);
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+        FormatTime((int)value);
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
         throw new NotImplementedException();
 
     public static string FormatTime(int seconds)
     {
-        var minutes = seconds / 60;
+        TimeSpan time = TimeSpan.FromSeconds(seconds);
 
-        return $"{(minutes < 10 ? "0" : "")}{minutes}:{((seconds % 60) < 10 ? "0" : "")}{seconds % 60}";
+        return time.ToString(@"mm\:ss");
     }
 }
