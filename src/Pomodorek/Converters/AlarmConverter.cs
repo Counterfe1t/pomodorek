8da81338583807dfabbc7560e2d2b9bf0c8ec049
Counterfe1t <karoljanbarcz@gmail.com>
2024-11-18 15:34:26 +0100

    Fix code formatting in AlarmConverter

--- src/Pomodorek/Converters/AlarmConverter.cs
+++ src/Pomodorek/Converters/AlarmConverter.cs
@@ -2,10 +2,17 @@
 
 public class AlarmConverter : IValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => FormatAlarm((DateTime)value);
+    public object Convert(
+        object value,
+        Type targetType,
+        object parameter,
+        CultureInfo culture) => FormatAlarm((DateTime)value);
 
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
-        throw new NotImplementedException();
+    public object ConvertBack(
+        object value,
+        Type targetType,
+        object parameter,
+        CultureInfo culture) => throw new NotImplementedException();
 
     public static string FormatAlarm(DateTime alarm) => alarm.ToLocalTime().ToString("HH:mm");
 }
