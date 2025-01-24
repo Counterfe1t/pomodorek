2a8cc2054476f1daf0b1987f4c5492c486fcbc0f
Counterfe1t <karoljanbarcz@gmail.com>
2025-01-25 00:06:44 +0100

    Create a custom button control

---
+++ src/Pomodorek/Views/Controls/ButtonControl.xaml.cs
@@ -0,0 +1,49 @@
+using System.Windows.Input;
+
+namespace Pomodorek.Views.Controls;
+
+[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class ButtonControl
+{
+	public static readonly BindableProperty TextProperty = BindableProperty.Create(
+		nameof(Text),
+		typeof(string),
+		typeof(ButtonControl),
+		default(string),
+		BindingMode.OneWay,
+		propertyChanged: (bindable, oldValue, newValue) =>
+		{
+			((ButtonControl)bindable).Button.Text = newValue as string;
+		});
+
+	public string Text
+	{
+		get => GetValue(TextProperty) as string;
+		set => SetValue(TextProperty, value);
+	}
+
+	public static readonly BindableProperty ButtonCommandProperty = BindableProperty.Create(
+		nameof(ButtonCommand),
+		typeof(ICommand),
+		typeof(ButtonControl),
+		default(ICommand),
+        BindingMode.OneWay);
+
+	public ICommand ButtonCommand
+	{
+		get => GetValue(ButtonCommandProperty) as ICommand;
+		set => SetValue(ButtonCommandProperty, value);
+	}
+
+    public ButtonControl()
+	{
+		InitializeComponent();
+		InitializeProperties();
+    }
+
+	private void InitializeProperties()
+	{
+        Button.Text = Text;
+        Button.Command = new Command(() => ButtonCommand.Execute(null));
+    }
+}
