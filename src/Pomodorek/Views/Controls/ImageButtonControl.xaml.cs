e7b079d161417159a2dd8fcb6328ff48344b38b5
Counterfe1t <karoljanbarcz@gmail.com>
2025-03-02 23:21:01 +0100

    Add dummy custom image button control for later implementation

---
+++ src/Pomodorek/Views/Controls/ImageButtonControl.xaml.cs
@@ -0,0 +1,10 @@
+namespace Pomodorek.Views.Controls;
+
+[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class ImageButtonControl
+{
+    public ImageButtonControl()
+    {
+        InitializeComponent();
+    }
+}
