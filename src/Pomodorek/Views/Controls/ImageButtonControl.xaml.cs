37f6e1f71131029db037e3568cacb95147fc25bc
Counterfe1t <karoljanbarcz@gmail.com>
2025-03-06 17:35:49 +0100

    Add bindable properties to custom image button control for displaying image source and handling on click events

--- src/Pomodorek/Views/Controls/ImageButtonControl.xaml.cs
+++ src/Pomodorek/Views/Controls/ImageButtonControl.xaml.cs
@@ -3,8 +3,45 @@ namespace Pomodorek.Views.Controls;
 [XamlCompilation(XamlCompilationOptions.Compile)]
 public partial class ImageButtonControl
 {
+    public static readonly BindableProperty SourceProperty = BindableProperty.Create(
+        nameof(Source),
+        typeof(string),
+        typeof(ImageButtonControl),
+        default(string),
+        BindingMode.OneWay,
+        propertyChanged: (bindable, oldValue, newValue) =>
+        {
+            ((ImageButtonControl)bindable).ImageButton.Source = newValue as string;
+        });
+
+    public string Source
+    {
+        get => GetValue(SourceProperty) as string;
+        set => SetValue(SourceProperty, value);
+    }
+
+    public static readonly BindableProperty ImageButtonCommandProperty = BindableProperty.Create(
+        nameof(ImageButtonCommand),
+        typeof(RelayCommand),
+        typeof(ImageButtonControl),
+        default(RelayCommand),
+        BindingMode.OneWay);
+
+    public RelayCommand ImageButtonCommand
+    {
+        get => GetValue(ImageButtonCommandProperty) as RelayCommand;
+        set => SetValue(ImageButtonCommandProperty, value);
+    }
+
     public ImageButtonControl()
     {
         InitializeComponent();
+        InitializeProperties();
+    }
+
+    private void InitializeProperties()
+    {
+        ImageButton.Source = Source;
+        ImageButton.Command = new Command(() => ImageButtonCommand.Execute(null));
     }
 }
