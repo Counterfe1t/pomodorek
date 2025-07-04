b417e8f0f50b96684a3d8a1b8962ae5b96548789
Counterfe1t <karoljanbarcz@gmail.com>
2025-07-05 00:11:31 +0200

    Add base content page class

---
+++ src/Pomodorek/Views/Base/ContentPageBase.cs
@@ -0,0 +1,15 @@
+﻿namespace Pomodorek.Views.Base;
+
+public abstract class ContentPageBase : ContentPage
+{
+    protected ContentPageBase()
+    {
+        NavigationPage.SetBackButtonTitle(this, string.Empty);
+    }
+
+    // TODO: Execute initialization logic here
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+    }
+}
